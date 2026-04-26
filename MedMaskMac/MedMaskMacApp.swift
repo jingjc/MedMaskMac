160aa39499d313e74c3cae445ab4b1b8ae0157e6
jcjing <jingjc@foxmail.com>
2026-04-26 11:15:31 +0800

    Initial Commit

---
+++ MedMaskMac/MedMaskMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedMaskMacApp.swift
+//  MedMaskMac
+//
+//  Created by jcjing on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MedMaskMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
